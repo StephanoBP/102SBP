75155bca059c55c8650f1b15527d47cdeba3a84a
Your Name <Stephanoburgos80@gmail.com>
2022-10-13 09:52:59 +0200

    Ejercicio3

---
+++ 102SBP_03.js
@@ -0,0 +1,30 @@
+let contactos = [
+    {nombre : "Maxwell Wright",teléfono : "(0191) 719 6495", correo : "Curabitur.egestas.nunc@nonummyac.co.uk"},
+    {nombre : "Raja Villarreal",teléfono : "0866 398 2895", correo : "posuere.vulputate@sed.com"},
+    {nombre : "Helen Richards",teléfono : "0800 1111", correo : "libero@convallis.edu"}
+]
+contactos.push({nombre : "Maisie Haley", teléfono : "0913 531 3030", correo : "risus.Quisque@urna.ca"});
+
+let nombres,teléfonos,correos;
+
+nombres = String(prompt("Dame tu nombre"));
+teléfonos = String(prompt("Dame tu teléfono"));
+correos = String(prompt("Dame tu correo"));
+contactos.push({nombre : nombres, teléfono : teléfonos, correo : correos});
+
+for(i = 0 ;i<contactos.length;i++){
+    if(i==0||i==contactos.length-1){
+        document.write("<p>");
+        alert(contactos[i].nombre+" / " + contactos[i].teléfono+" / "+ contactos[i].correo);
+        console.log(contactos[i].nombre+" / " + contactos[i].teléfono+" / "+ contactos[i].correo);
+        for(let a in contactos[i]){
+            if(contactos[i][a]!=contactos[i].correo){
+                document.write(contactos[i][a]+" / ");
+            }else{
+                document.write(contactos[i][a]);
+            }
+            
+        }
+        document.write("</p>");
+    }
+}
