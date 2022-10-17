3afeb2bc610ea8bb288485f36c4a91562a810c0b
Your Name <Stephanoburgos80@gmail.com>
2022-10-17 12:33:41 +0200

    Actividad 3 2.0

--- 102SBP_03.js
+++ 102SBP_03.js
@@ -2,7 +2,7 @@ let contactos = [
     {nombre : "Maxwell Wright",teléfono : "(0191) 719 6495", correo : "Curabitur.egestas.nunc@nonummyac.co.uk"},
     {nombre : "Raja Villarreal",teléfono : "0866 398 2895", correo : "posuere.vulputate@sed.com"},
     {nombre : "Helen Richards",teléfono : "0800 1111", correo : "libero@convallis.edu"}
-]
+];
 contactos.push({nombre : "Maisie Haley", teléfono : "0913 531 3030", correo : "risus.Quisque@urna.ca"});
 
 let nombres,teléfonos,correos;
@@ -11,11 +11,11 @@ nombres = String(prompt("Dame tu nombre"));
 teléfonos = String(prompt("Dame tu teléfono"));
 correos = String(prompt("Dame tu correo"));
 contactos.push({nombre : nombres, teléfono : teléfonos, correo : correos});
-
-for(i = 0 ;i<contactos.length;i++){
+document.write("<h1 style=\"color:white;\">Mostrar el primer contacto y el último:</h1>");
+console.log("Mostrar el primer contacto y el último:");
+for(i=0;i<contactos.length;i++){
     if(i==0||i==contactos.length-1){
-        document.write("<p>");
-        alert(contactos[i].nombre+" / " + contactos[i].teléfono+" / "+ contactos[i].correo);
+        document.write("<p style=\"color:white;\">");
         console.log(contactos[i].nombre+" / " + contactos[i].teléfono+" / "+ contactos[i].correo);
         for(let a in contactos[i]){
             if(contactos[i][a]!=contactos[i].correo){
