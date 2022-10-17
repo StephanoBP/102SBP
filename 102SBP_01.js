aeb5bdcd4ee53e731bb70a17a4e822d5bc23ba3f
Your Name <Stephanoburgos80@gmail.com>
2022-10-17 12:28:58 +0200

    Actividad 1 2.0

--- 102SBP_01.js
+++ 102SBP_01.js
@@ -9,5 +9,5 @@ teléfono3="0800 1111";
 correo1="Curabitur.egestas.nunc@nonummyac.co.uk";
 correo2="posuere.vulputate@sed.com";
 correo3="libero@convallis.edu";
-
-window.alert(nombre1 + nombre2 + nombre3 + teléfono1 + teléfono2 + teléfono3 + correo1 + correo2 + correo3);
+console.log("Enseñando todas las variables:\nnombres: " + nombre1 + " / " + nombre2 + " / " + nombre3 + "\nteléfonos: " + teléfono1 + " / " + teléfono2 + " / " + teléfono3 + "\ncorreos: " + correo1 + " / " + correo2 + " / " + correo3);
+document.write("<h1 style=\"color:white;\">Enseñando todas las variables:</h1><p style=\"color:white;\">" + "nombres: " + nombre1 + " / " + nombre2 + " / " + nombre3 + "</br>teléfonos: " + teléfono1 + " / " + teléfono2 + " / " + teléfono3 + "<br>correos: " + correo1 + " / " + correo2 + " / " + correo3+"</p>");
