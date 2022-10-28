32eaf6dd7549a2f74898a754711a575890c4c102
Your Name <Stephanoburgos80@gmail.com>
2022-10-28 13:06:03 +0200

    TareaContinua5 FIN

--- 102SBP_05.js
+++ 102SBP_05.js
@@ -16,7 +16,7 @@ do{
         }
     }while(booleanos)
     if(opcion==1){
-        document.write("<h1 style=\"color:white;\">Petición "+petición +": Mostrar el primer contacto.</h1><p style=\"color:white;\">")
+        document.write("<h1>Petición "+petición +": Mostrar el primer contacto.</h1><p>")
         console.log("Petición "+petición +": Mostrar todos los contactos.\n"+contactos[0].nombre+" / " + contactos[0].teléfono+" / "+ contactos[0].correo);
         for(let a in contactos[0]){
             if(contactos[0][a]!=contactos[0].correo){
@@ -30,7 +30,7 @@ do{
         booleanos=true;
     }else if(opcion==2){
             console.log("Petición "+petición +": Mostrar todos los contactos.\n"+contactos[contactos.length-1].nombre+" / " + contactos[contactos.length-1].teléfono+" / "+ contactos[contactos.length-1].correo);
-            document.write("<h1 style=\"color:white;\">Petición "+petición +": Mostrar el último contacto.</h1><p style=\"color:white;\">")
+            document.write("<h1>Petición "+petición +": Mostrar el último contacto.</h1><p>")
             for(let a in contactos[contactos.length-1]){
                 if(contactos[contactos.length-1][a]!=contactos[contactos.length-1].correo){
                     document.write(contactos[contactos.length-1][a]+" / ");
@@ -42,10 +42,10 @@ do{
             document.write("</p>");
             booleanos=true;
     }else if(opcion==3){
-        document.write("<h1 style=\"color:white;\">Petición "+petición +": Mostrar todos los contactos.</h1>");
+        document.write("<h1>Petición "+petición +": Mostrar todos los contactos.</h1>");
         console.log("Petición "+petición +": Mostrar todos los contactos.");
-        for(i=0;i<contactos.length;i++){
-            document.write("<p style=\"color:white;\">");
+        for(let i=0;i<contactos.length;i++){
+            document.write("<p>");
             console.log(contactos[i].nombre+" / " + contactos[i].teléfono+" / "+ contactos[i].correo);
             for(let a in contactos[i]){
                 if(contactos[i][a]!=contactos[i].correo){
@@ -56,21 +56,21 @@ do{
                 
             }
             document.write("</p>");
-            booleanos=true;
         }    
+        booleanos=true;
     }else if(opcion==4){
         let nombres=prompt("Dime un nombre");
-        let teléfonos=prompt("Dime un numero");
+        let teléfonos=prompt("Dime un número");
         let correos=prompt("Dime un correo");
         console.log("Petición "+petición +": Añadir nuevo contacto.\nHas introducido los siguientes datos:\nnombre: "+ nombres + "\nteléfono: " + teléfonos + "\ncorreo: " + correos);
         if(nombres!="" && teléfonos!="" && correos!=""){
             contactos.push({nombre : nombres, teléfono : teléfonos, correo : correos});
-            document.write("<h1 style=\"color:white;\">Petición "+petición +": Añadir nuevo contacto.</h1><p style=\"color:white;\"> Has introducido los siguientes datos:<br>nombre: "+ nombres + "<br>teléfono: " + teléfonos + "<br>correo: " + correos+"</p>");
+            document.write("<h1>Petición "+petición +": Añadir nuevo contacto.</h1><p> Has introducido los siguientes datos:<br>nombre: "+ nombres + "<br>teléfono: " + teléfonos + "<br>correo: " + correos+"</p>");
         }
         booleanos=true;
     }else if(opcion==5){
-        console.log("Petición "+petición +":Salir del programa.\nHas salido correctamente del sistema, buen día.");
-        document.write("<h1 style=\"color:white;\">Petición "+petición +": Salir del programa.</h1><p style=\"color:white;\">Has salido correctamente del sistema, buen día.</p>")
+        console.log("Petición "+petición +": Salir del programa.\nHas salido correctamente del sistema, buen día.");
+        document.write("<h1>Petición "+petición +": Salir del programa.</h1><p>Has salido correctamente del sistema, buen día.</p>")
         booleanos=false;
     }
 }while(booleanos);
