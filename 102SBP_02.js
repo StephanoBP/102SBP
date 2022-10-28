5e78c353fd67a21c9634a9f661d405986b6b1927
Stephano <stephanoburgos80@gmail.com>
2022-10-28 19:08:44 +0200

    Ejercicio 2 Final

--- 102SBP_02.js
+++ 102SBP_02.js
@@ -1,22 +1,9 @@
+//Matriz Contactos
 let contactos = [
     {nombre : "Maxwell Wright",teléfono : "(0191) 719 6495", correo : "Curabitur.egestas.nunc@nonummyac.co.uk"},
     {nombre : "Raja Villarreal",teléfono : "0866 398 2895", correo : "posuere.vulputate@sed.com"},
     {nombre : "Helen Richards",teléfono : "0800 1111", correo : "libero@convallis.edu"}
 ]
 contactos.push({nombre : "Maisie Haley", teléfono : "0913 531 3030", correo : "risus.Quisque@urna.ca"});
-for(let i = 0 ;i<contactos.length;i++){
-    if(i==0||i==contactos.length-1){
-        if(i==0)document.write("<h1>Mostrar el primer contacto.</h1><p>");
-        else{document.write("<h1>Mostrar el último contacto.</h1><p>");}
-        console.log(contactos[i].nombre+" / " + contactos[i].teléfono+" / "+ contactos[i].correo);
-        for(let a in contactos[i]){
-            if(contactos[i][a]!=contactos[i].correo){
-                document.write(contactos[i][a]+" / ");
-            }else{
-                document.write(contactos[i][a]);
-            }
-            
-        }
-        document.write("</p>");
-    }
-}
+console.log("Mostrar el primer contacto:\n"+contactos[0].nombre+" / " + contactos[0].teléfono+" / "+ contactos[0].correo+"\nMostrar el último contacto:\n"+contactos[contactos.length-1].nombre+" / " + contactos[contactos.length-1].teléfono+" / "+ contactos[contactos.length-1].correo);
+document.write("<h1>Mostrar el primer contacto.</h1><p>"+contactos[0].nombre+" / " + contactos[0].teléfono+" / "+ contactos[0].correo+"</p><h1>Mostrar el último contacto.</h1><p>"+contactos[contactos.length-1].nombre+" / " + contactos[contactos.length-1].teléfono+" / "+ contactos[contactos.length-1].correo+"</p>");
