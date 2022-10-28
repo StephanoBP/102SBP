9b746a82f36c6812db91866e5e52d3f192dd7114
Your Name <Stephanoburgos80@gmail.com>
2022-10-28 13:01:34 +0200

    TareaContinua1 FIN

--- 102SBP_01.js
+++ 102SBP_01.js
@@ -9,5 +9,5 @@ teléfono3="0800 1111";
 correo1="Curabitur.egestas.nunc@nonummyac.co.uk";
 correo2="posuere.vulputate@sed.com";
 correo3="libero@convallis.edu";
-console.log("Enseñando todas las variables:\nnombres: " + nombre1 + " / " + nombre2 + " / " + nombre3 + "\nteléfonos: " + teléfono1 + " / " + teléfono2 + " / " + teléfono3 + "\ncorreos: " + correo1 + " / " + correo2 + " / " + correo3);
-document.write("<h1 style=\"color:white;\">Enseñando todas las variables:</h1><p style=\"color:white;\">" + "nombres: " + nombre1 + " / " + nombre2 + " / " + nombre3 + "</br>teléfonos: " + teléfono1 + " / " + teléfono2 + " / " + teléfono3 + "<br>correos: " + correo1 + " / " + correo2 + " / " + correo3+"</p>");
+console.log("Enseñando todas las variables:\nNombres: " + nombre1 + " / " + nombre2 + " / " + nombre3 + "\nTeléfonos: " + teléfono1 + " / " + teléfono2 + " / " + teléfono3 + "\nCorreos: " + correo1 + " / " + correo2 + " / " + correo3);
+document.write("<h1>Enseñando todas las variables:</h1><p>" + "<b>Nombres:</b> " + nombre1 + " / " + nombre2 + " / " + nombre3 + "</br><b>Teléfonos:</b> " + teléfono1 + " / " + teléfono2 + " / " + teléfono3 + "<br><b>Correos:</b> " + correo1 + " / " + correo2 + " / " + correo3+"</p>");
